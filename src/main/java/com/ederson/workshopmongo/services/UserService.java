377b9a548bc146bb24191db40af95377b09a5e63
ECRocha <edersonclaudiodarocha@hotmail.com>
2024-01-02 16:07:15 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ederson/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.ederson.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ederson.workshopmongo.domain.User;
+import com.ederson.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
